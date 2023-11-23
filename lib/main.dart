da456447b3fdf06ab24e34fdede89c3370837300
ChauDz <buichau40@gmail.com>
2023-11-23 10:04:37 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:git/models/student.dart';
-import 'package:git/pages/page2.dart';
 import 'package:git/routes/routes.dart';
 
 void main() {
@@ -19,20 +18,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      //onGenerateRoute là một phương thức trong MaterialApp của Flutter,
-      // được sử dụng để định nghĩa một hàm callback mà framework gọi khi
-      // cố gắng điều hướng đến một route mà không có route được xác định trước đó. Điều này cung cấp cho bạn khả năng xử lý các trang không xác định trước đó và quyết định làm thế nào để xây dựng và
-      // hiển thị chúng
-      onGenerateRoute: (settings){
-        print('onGenerateRoute is called');
-        print('onGenerateRoute is called');
-        print(settings.name);
-        if(settings.name == '/page2'){
-          Student s = settings.arguments as Student;
-          print(s?.getAge());
-          return MaterialPageRoute(builder: (context)=> Page2(age: s?.getAge(),name: s?.getName(),numberPhone: s?.getNumberPhone(),));
-        }
-      },
       initialRoute: '/',
       routes: routes,
     );
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,9 +1,7 @@
-import 'package:git/pages/intropage.dart';
-import 'package:git/pages/page1.dart';
-import 'package:git/pages/page2.dart';
+import 'package:git/pages/homepage.dart';
+import 'package:git/pages/secondPage.dart';
 
 var routes = {
-  '/': (context) => const IntroPage(),
-  '/page1': (context) => const Page1(),
-  // '/page2': (context) => const Page2()
+  '/': (context) => const HomePage(),
+  '/secondpage': (context) => const SecondPage(),
 };
